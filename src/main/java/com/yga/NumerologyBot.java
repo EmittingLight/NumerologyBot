9d545b2ad8a78a27fcff2e0468229e1dad079bd0
SergeiPetrov1974 <58001369+SergeiPetrov1974@users.noreply.github.com>
2023-05-25 11:50:29 +0300

    ok

--- src/main/java/com/yga/NumerologyBot.java
+++ src/main/java/com/yga/NumerologyBot.java
@@ -344,6 +344,6 @@ public class NumerologyBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return "5656643323:AAE8bEZ5kI1wfSf-dNAdHqqjU_2WQXFIjhM";
+        return "";
     }
 }
